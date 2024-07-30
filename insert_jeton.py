35867942edb146657c8cf60e9422b39103565086
gvw332 <108952889+gvw332@users.noreply.github.com>
2024-07-30 16:24:04 +0200

    Update insert_jeton.py

--- insert_jeton.py
+++ insert_jeton.py
@@ -1,9 +1,3 @@
-def display_board(board):
-    # Affiche chaque ligne du plateau
-    for row in board:
-        print(' '.join(row))
-    print()  # Nouvelle ligne à la fin de l'affichage du plateau
-
 def drop_piece(board, col, piece):
     # Parcourt les lignes du bas vers le haut pour trouver la première cellule vide
     for row in reversed(board):
