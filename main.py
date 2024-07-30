3709aafd6b868b4828cdc2d6b016d0517c3c924f
Fohn-4 <renardstaifane@gmail.com>
2024-07-30 10:39:58 +0200

    IMP: Game loop
    egrtg

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+
+
+
 cols = 7
 rows = 6
 
+run = True
+
+yellowPlayer = '🟡'
+redPlayer = '🔴'
+
 board = [['⚫' for _ in range(cols)] for _ in range(rows)]
     
-for row in board:
-    print(row)
+
+while run:
+    for row in board:
+        print(row)
+
+    keepGoing = input('''Continue ? 
+                        Y for yes
+                        N for no
+                        ''')
+    if keepGoing == 'Y':
+        run = True
+    elif keepGoing == 'N':
+        run = False
+    else:
+        pass
+    
+
